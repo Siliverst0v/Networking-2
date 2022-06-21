45b8fae69f51152e2620302af3cafa85601296e5
Siliverst0v <94259002+Siliverst0v@users.noreply.github.com>
2022-06-21 19:20:45 +0300

    Initial Commit

---
+++ Networking-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking-2
+//
+//  Created by Анатолий Силиверстов on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
